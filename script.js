d443c4f5f7b284f968d6a349aca503a594552660
FranzCalore <franzkaiser1993@gmail.com>
2022-11-10 15:51:36 +0100

    terminata la prima stesura completa

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-let chilometriDaPercorrere=prompt("Quanti chilometri devi percorrere?");
-let etaPasseggero=prompt("Quanti anni ha il passeggero?");
+let chilometriDaPercorrere;
+let etaPasseggero;
 let sconto;
 let prezzoBiglietto;
 
-prezzoBiglietto = (chilometriDaPercorrere*0.21);
-if (etaPasseggero>=65){
-    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto/100*40);
-}
-else if (etaPasseggero<18) {
-    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto/100*20);
-}
+let bottone = document.getElementById("bottoneCalcolo")
+bottone.addEventListener("click", function () {
 
-prezzoBiglietto = prezzoBiglietto.toFixed(2);
-console.log (prezzoBiglietto + "€");
+    chilometriDaPercorrere=document.getElementById("chilometriBiglietto").value
+    etaPasseggero=document.getElementById("etaPasseggero").value
+
+    prezzoBiglietto = (chilometriDaPercorrere * 0.21);
+    if (etaPasseggero >= 65) {
+        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto / 100 * 40);
+        document.getElementById("ticket-head").classList.remove("bg-danger");
+        document.getElementById("ticket-head").classList.add("bg-warning");
+        document.getElementById("ticket-head").innerHTML = "<b> Railways Ticket - Senior </b>";
+        document.getElementById("ticket-foot").classList.remove("bg-danger");
+        document.getElementById("ticket-foot").classList.add("bg-warning");
+        document.getElementById("bottoneCalcolo").classList.remove("btn-danger")
+        document.getElementById("bottoneCalcolo").classList.add("btn-warning")
+    }
+    else if (etaPasseggero < 18) {
+        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto / 100 * 20);
+        document.getElementById("ticket-head").classList.remove("bg-danger");
+        document.getElementById("ticket-head").classList.add("bg-success");
+        document.getElementById("ticket-head").innerHTML = "<b> Railways Ticket - Junior </b>";
+        document.getElementById("ticket-foot").classList.remove("bg-danger");
+        document.getElementById("ticket-foot").classList.add("bg-success");
+        document.getElementById("bottoneCalcolo").classList.remove("btn-danger")
+        document.getElementById("bottoneCalcolo").classList.add("btn-success")
+    }
+
+    prezzoBiglietto = prezzoBiglietto.toFixed(2);
+    prezzoBiglietto = prezzoBiglietto+"€"
+    console.log(prezzoBiglietto);
+
+    
+    document.getElementById("costoBiglietto").innerHTML="Il prezzo del tuo biglietto è: " + prezzoBiglietto;
+})
+
